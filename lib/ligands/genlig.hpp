ca316ced78249ffb8babe796f0c0b41a0dadd549
Jonathan Fine <finej@purdue.edu>
2016-06-22 21:07:23 -0400

    Fixed Pointer Issue

--- lib/ligands/genlig.hpp
+++ lib/ligands/genlig.hpp
@@ -12,11 +12,15 @@ namespace genlig {
 		const string &lig_file, const string &bsite_file);
 	pair<BindingSiteClusters, BindingSiteScores> generate_binding_site_prediction(const string &json_with_ligs_file, 
 		const string &bio_dir, const int num_bsites);
-    
-	// Operator overloading for typedef types can lead to issues.
-	// See http://blog.mezeske.com/?p=170 for details.
+
+}
+
+// Operator overloading for typedef types can lead to issues.
+// See http://blog.mezeske.com/?p=170 for details.
+namespace Molib {
 	ostream& operator<<(ostream& os, const map<int, Molib::Molecules>& bsites);
-	ostream& operator<<(ostream& os, const map<int, double>& bscores);
-};
+}
+
+ostream& operator<<(ostream& os, const map<int, double>& bscores);
 
 #endif
