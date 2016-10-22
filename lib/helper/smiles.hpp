e64564fd3c1904f4ffeb1c85fbc4e8eed38b61ec
Jonathan Fine <finej@purdue.edu>
2016-10-22 14:05:55 -0400

    Forgot some files

---
+++ lib/helper/smiles.hpp
@@ -0,0 +1,27 @@
+#ifndef SMILES_H
+#define SMILES_H
+
+#include <vector>
+#include <string>
+#include <ostream>
+
+using namespace std;
+
+namespace help {
+	struct edge { 
+		string atom_property1;
+		string atom_property2;
+		string bond_property;
+	};
+	typedef vector<edge> smiles;
+	struct rename_rule {
+		smiles pattern;
+		vector<string> rule;
+	};
+	typedef vector<rename_rule> rename_rules;
+
+	ostream& operator<<(ostream& os, const smiles& edges);
+	ostream& operator<<(ostream& os, const rename_rule& rule);
+}
+
+#endif
