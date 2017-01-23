176f0cdbf6be7d151ea1ceced8a0d82ecdde2a49
Brandon Stewart <stewartb2@gmail.com>
2017-01-23 08:55:08 -0500

    More changes to cuda_linker

--- lib/linker/cuda_linker.cu
+++ lib/linker/cuda_linker.cu
@@ -1,8 +1,22 @@
 #include "cuda_linker.h"
 #include "cuda.h"
 #include "cuda_runtime.h"
-#include "linker.hpp"
 
+#include "linker.hpp"
+#include "poses.hpp"
+#include "geom3d/quaternion.hpp"
+#include "score/score.hpp"
+#include "pdbreader/nrset.hpp"
+#include "pdbreader/bond.hpp"
+#include "helper/benchmark.hpp"
+#include "helper/help.hpp"
+#include "helper/array2d.hpp"
+#include "graph/mcqd.hpp"
+#include "modeler/modeler.hpp"
+#include "geom3d/geom3d.hpp"
+#include "cluster/greedy.hpp"
+#include <queue>
+#include <iostream>
 __global__ void find_good_neighbor() {
 
 }
