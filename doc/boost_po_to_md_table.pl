d6d3b10b5a5f55b09c6a13fb31ce12466e28d49c
Jonathan Fine <finej@purdue.edu>
2017-02-26 18:10:58 -0500

    Fixed indentation in translation script

--- doc/boost_po_to_md_table.pl
+++ doc/boost_po_to_md_table.pl
@@ -10,58 +10,58 @@ my @variable_list = ();
 
 while (<>) {
 
-        chomp;
+    chomp;
 
-        if ( $_ =~ m/^$/ ) {
-                next;
-        }
+    if ( $_ =~ m/^$/ ) {
+        next;
+    }
 
-        if ( $_ =~ m/^\S/ ) {
-                $current_variable = $_;
-                $variable_group{ $_ } = {};
-                push @variable_list, $_;
-                next;
-        }
+    if ( $_ =~ m/^\S/ ) {
+        $current_variable = $_;
+        $variable_group{ $_ } = {};
+        push @variable_list, $_;
+        next;
+    }
         
-        next if $current_variable eq "Generic options:";
+    next if $current_variable eq "Generic options:";
+    
+    if ( $_ =~ m/^\s+\-\-(\S+)\s\[=arg\(=(\S+)\)\](?:\s+(\S.*))?/x ) {
+        $current_option = $1;
+        $variable_group{ $current_variable }->{$current_option} = {def_value=>$2.' (Implicit)', desc=>$3};
+        next;
+    }
 
-        if ( $_ =~ m/^\s+\-\-(\S+)\s\[=arg\(=(\S+)\)\](?:\s+(\S.*))?/x ) {
-                $current_option = $1;
-                $variable_group{ $current_variable }->{$current_option} = {def_value=>$2.' (Implicit)', desc=>$3};
-                next;
-        }
+    if ( $_ =~ m/^\s+\-\-(\S+)\sarg\s\(=(\S+)\)(?:\s+(\S.*))?/x ) {
+        $current_option = $1;
+        $variable_group{ $current_variable }->{$current_option} = {def_value=>$2, desc=>$3};
+        next;
+    }
 
-        if ( $_ =~ m/^\s+\-\-(\S+)\sarg\s\(=(\S+)\)(?:\s+(\S.*))?/x ) {
-                $current_option = $1;
-                $variable_group{ $current_variable }->{$current_option} = {def_value=>$2, desc=>$3};
-                next;
-        }
+    if ( $_ =~ m/^\s+\-\-(\S+)\sarg\s+(\S.*)/x ) {
+        $current_option = $1;
+        $variable_group{ $current_variable }->{$current_option} = {def_value=>'~None~', desc=>$2};
+        next;
+    }
 
-        if ( $_ =~ m/^\s+\-\-(\S+)\sarg\s+(\S.*)/x ) {
-                $current_option = $1;
-                $variable_group{ $current_variable }->{$current_option} = {def_value=>'~None~', desc=>$2};
-                next;
-        }
-
-        if ( $_ =~ m/^\s+(\S.*)/) {
-                $variable_group{ $current_variable }->{$current_option}->{desc}.=$1;
-	}
+    if ( $_ =~ m/^\s+(\S.*)/) {
+        $variable_group{ $current_variable }->{$current_option}->{desc}.=$1;
+    }
 }
 
 for my $key (@variable_list) {
 
-        next if $key eq "Generic options:";
+    next if $key eq "Generic options:";
 
-        my $value = $variable_group{ $key };
+    my $value = $variable_group{ $key };
 
-        print $key, "\n\n";
+    print $key, "\n\n";
 
-        print "|Option Name | Default | Description |\n";
-        print "|------------|--------| ----------------------------- |\n";
+    print "|Option Name | Default | Description |\n";
+    print "|------------|--------| ----------------------------- |\n";
 
-        while ( (my $key2, my $value2) = each %{$value} ) {
-                print "| $key2 | $value2->{def_value} | $value2->{desc} |\n"
-        }
+    while ( (my $key2, my $value2) = each %{$value} ) {
+        print "| $key2 | $value2->{def_value} | $value2->{desc} |\n"
+    }
 
-        print "\n";
+    print "\n";
 }
