cec9ec2e1626d3592295d03659b9cf0948e0ad7b
Jonathan Fine <finej@purdue.edu>
2017-07-07 09:54:55 -0400

    Small fixes

--- scripts/dock_multiple.sh
+++ scripts/dock_multiple.sh
@@ -8,33 +8,36 @@ then
     exit 1
 fi
 
-command=$1
+__command=$1
 
-working_dir=$2
-prot_list=$working_dir/all.lst
+__working_dir=$2
+__prot_list=$__working_dir/all.lst
 
 shift 2
 
-export CANDOCK_prep=$working_dir/compounds/prepared_ligands.pdb
-export CANDOCK_seeds=$working_dir/compounds/seeds.txt
+export CANDOCK_prep=$__working_dir/compounds/prepared_ligands.pdb
+export CANDOCK_seeds=$__working_dir/compounds/seeds.txt
+export CANDOCK_seeds_pdb=$__working_dir/compounds/seeds.pdb
 
-: ${limit:=1000}
-: ${start:=1}
+: ${MCANDOCK_relaunch:=0}
 
-count=0
-current=1
+: ${MCANDOCK_limit:=1000}
+: ${MCANDOCK_start:=1}
 
-for i in `cat $prot_list`
+__count=0
+__current=1
+
+for i in `cat $__prot_list`
 do
 
-    if [[ "$count" -ge "$limit" ]]
+    if [[ "$__count" -ge "$MCANDOCK_limit" ]]
     then
         break
     fi
 
-    if [[ "$current" -lt "$start" ]]
+    if [[ "$__current" -lt "$MCANDOCK_start" ]]
     then
-        current=$((current+1))
+        __current=$((__current+1))
         continue
     fi
 
@@ -45,13 +48,13 @@ do
     if [[ ! -d $i ]]
     then
         mkdir $i
-    else
+    elif [[ "$MCANDOCK_relaunch" -eq "0" ]]
         echo "Warning: previous run for $i, using previous results"
         continue
     fi
 
-    $MCANDOCK_MOD_PATH/submit_candock_module.sh $command -N $(basename $working_dir)_$i $([[ ! -z "$@" ]] && echo "$@")
+    $MCANDOCK_MOD_PATH/submit_candock_module.sh $__command -N $(basename $__working_dir)_$i $([[ ! -z "$@" ]] && echo "$@") -o $i/$(basename $working_dir)_${i}_output.log -e $i/$(basename $working_dir)_${i}_errors.log
 
-    count=$((count+1))
+    __count=$((__count+1))
 
 done
---
+++ src/cd_dynamics_run.cpp
@@ -0,0 +1,109 @@
+#include "program/cmdlnopts.hpp"
+#include "program/target.hpp"
+
+#include "score/score.hpp"
+#include "modeler/forcefield.hpp"
+#include "modeler/modeler.hpp"
+
+#include "version.hpp"
+#include "drm/drm.hpp"
+
+using namespace std;
+using namespace Program;
+
+////////////////// TEST SINGLEPOINT ENERGY CALCULATION OF COMPLEX ///////////////////////////
+
+int main(int argc, char* argv[]) {
+        try {
+                if(!drm::check_drm(Version::get_install_path() + "/.candock")) {
+                    throw logic_error("CANDOCK has expired. Please contact your CANDOCK distributor to get a new version.");
+                }
+
+                Inout::Logger::set_all_stderr(true);
+
+                help::Options::set_options(new Program::CmdLnOpts(
+                    argc, argv, Program::CmdLnOpts::STARTING |
+                                Program::CmdLnOpts::FORCE_FIELD| 
+                                Program::CmdLnOpts::SCORING));
+
+                cerr << Version::get_banner()   <<
+                        Version::get_version()  <<
+                        Version::get_run_info();
+                cerr << help::Options::get_options()->configuration_file() << endl;
+
+                Molib::Molecules starting_mol;
+
+                Parser::FileParser drpdb (cmdl.get_string_option("receptor"),
+                                          Parser::pdb_read_options::first_model);
+
+                drpdb.parse_molecule(starting_mol);
+
+                Molib::Score score(cmdl.get_string_option("ref"), cmdl.get_string_option("comp"),
+                                   cmdl.get_string_option("func"),cmdl.get_int_option("cutoff"),
+                                   cmdl.get_double_option("step"));
+
+                score.define_composition(starting_mol.get_idatm_types(),
+                                         starting_mol.get_idatm_types())
+                     .process_distributions_file(cmdl.get_string_option("dist"))
+                     .compile_scoring_function()
+                     .parse_objective_function(cmdl.get_string_option("obj_dir"), cmdl.get_double_option("scale"));
+
+                OMMIface::ForceField ffield;
+
+                ffield.parse_gaff_dat_file(cmdl.get_string_option("gaff_dat"))
+                      .add_kb_forcefield(score, cmdl.get_double_option("step"), 15)
+                      .parse_forcefield_file(cmdl.get_string_option("amber_xml"))
+                      .parse_forcefield_file(cmdl.get_string_option("water_xml"));
+
+                if ( ! cmdl.get_string_option("gaff_heme").empty() ) {
+                        dbgmsg( "Adding " << cmdl.get_string_option("gaff_heme") << endl);
+                        ffield.parse_gaff_dat_file(cmdl.get_string_option("gaff_heme"));
+                }
+
+                OMMIface::SystemTopology::loadPlugins();
+
+                Molib::Molecule& starting = starting_mol[0];
+
+                Molib::Atom::Grid gridrec(starting.get_atoms());                        
+                starting_mol[0].prepare_for_mm(ffield, gridrec);
+
+                ffield.insert_topology(starting);
+
+                OMMIface::Modeler modeler (ffield, cmdl.get_string_option("fftype"),   cmdl.get_int_option("cutoff"),
+                                                   cmdl.get_double_option("mini_tol"), cmdl.get_int_option("max_iter"),
+                                                   cmdl.get_int_option("update_freq"), cmdl.get_double_option("pos_tol"),
+                                                   false, cmdl.get_double_option("dynamic_step_size"),
+                                                   cmdl.get_double_option("temperature"), cmdl.get_double_option("friction") );
+
+                modeler.add_topology (starting.get_atoms());
+
+                if (cmdl.get_string_option("integrator") == "verlet") {
+                        modeler.init_openmm(OMMIface::SystemTopology::integrator_type::verlet);
+                } else if (cmdl.get_string_option("integrator") == "langevin") {
+                        modeler.init_openmm(OMMIface::SystemTopology::integrator_type::langevin);
+                } else if (cmdl.get_string_option("integrator") == "brownian") {
+                        modeler.init_openmm(OMMIface::SystemTopology::integrator_type::brownian);
+                } else {
+                        throw Error("You need to select a valid integrator for dynamics");
+                }
+
+                modeler.add_crds (starting.get_atoms(), starting.get_crds());
+
+                modeler.init_openmm_positions();
+
+                modeler.set_num_steps_to_run(cmdl.get_int_option("dynamic_steps"));
+                modeler.dynamics();
+
+                // init with minimized coordinates
+                Molib::Molecule final_state (starting, modeler.get_state (starting.get_atoms()));
+
+                final_state.undo_mm_specific();
+
+                std::cout << final_state;
+
+        } catch (exception& e) {
+                cerr << e.what() << endl;
+        }
+        return 0;
+}
+
