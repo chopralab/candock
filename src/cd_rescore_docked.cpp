79464a46b06e424d4f10ad00e86b7e2d8f310f40
Jonathan Fine <finej@purdue.edu>
2017-07-07 11:57:06 -0400

    Rewrite of rescore_docked, it's very fast now.

--- src/cd_rescore_docked.cpp
+++ src/cd_rescore_docked.cpp
@@ -5,6 +5,9 @@
 #include "version.hpp"
 #include "drm/drm.hpp"
 
+#include "score/score.hpp"
+#include "modeler/forcefield.hpp"
+
 using namespace std;
 using namespace Program;
 
@@ -17,25 +20,59 @@ int main(int argc, char* argv[]) {
                     throw logic_error("CANDOCK has expired. Please contact your CANDOCK distributor to get a new version.");
                 }
 
+                Inout::Logger::set_all_stderr(true);
+
                 help::Options::set_options(new Program::CmdLnOpts(
                     argc, argv, Program::CmdLnOpts::STARTING |
-                                Program::CmdLnOpts::LIG_FRAMGENT| 
+                                Program::CmdLnOpts::FORCE_FIELD| 
                                 Program::CmdLnOpts::SCORING));
 
-                Benchmark main_timer;
-                main_timer.display_time("Starting");
-
-                cout << Version::get_banner()   <<
+                cerr << Version::get_banner()   <<
                         Version::get_version()  <<
                         Version::get_run_info();
-                cout << help::Options::get_options()->configuration_file() << endl;
+                cerr << help::Options::get_options()->configuration_file() << endl;
 
-                Program::FragmentLigands ligand_fragmenter;
-                ligand_fragmenter.run_step();
+                Molib::Molecules receptor_mols;
+                Molib::Molecules ligand_mols;
 
-                Program::Target targets(cmdl.get_string_option("receptor"));
-                targets.rescore_docked(ligand_fragmenter);
+                Parser::FileParser drpdb (cmdl.get_string_option("receptor"),
+                                          Parser::pdb_read_options::protein_poses_only |
+                                          Parser::pdb_read_options::all_models);
 
+                drpdb.parse_molecule(receptor_mols);
+
+                // Check to see if the user set the ligand option, use the receptor as a last resort.
+                const std::string &ligand_file = Inout::file_size(cmdl.get_string_option("ligand")) == 0 ?
+                                                    cmdl.get_string_option("receptor") :
+                                                    cmdl.get_string_option("ligand");
+
+                Parser::FileParser dlpdb (ligand_file,
+                                          Parser::pdb_read_options::docked_poses_only |
+                                          Parser::pdb_read_options::skip_atom |
+                                          Parser::pdb_read_options::all_models);
+
+                dlpdb.parse_molecule(ligand_mols);
+
+                Molib::Score score(cmdl.get_string_option("ref"), cmdl.get_string_option("comp"),
+                                   cmdl.get_string_option("func"),cmdl.get_int_option("cutoff"),
+                                   cmdl.get_double_option("step"));
+
+                score.define_composition(receptor_mols.get_idatm_types(),
+                                         ligand_mols.get_idatm_types())
+                     .process_distributions_file(cmdl.get_string_option("dist"))
+                     .compile_scoring_function();
+
+                for (size_t i = 0; i < receptor_mols.size(); ++i) {
+                        
+                        Molib::Molecule& protein = receptor_mols[i];
+                        Molib::Molecule& ligand  = ligand_mols[i];
+
+                        Molib::Atom::Grid gridrec(protein.get_atoms());                        
+
+                        const double energy = score.non_bonded_energy (gridrec, ligand);
+                        cout << energy << endl;
+
+                }
         } catch (exception& e) {
                 cerr << e.what() << endl;
         }
