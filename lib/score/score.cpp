33f1121e2e0200e4c024b73901fce9ec08b9593a
Jonathan Fine <finej@purdue.edu>
2017-09-23 14:43:22 -0400

    Overhaul of the rep/pot vectors to fix ob1 error

--- lib/score/score.cpp
+++ lib/score/score.cpp
@@ -401,15 +401,30 @@ namespace Score {
 					<< " end slope idx = " << *slope.rbegin()
 				);
 	
-				dbgmsg("energies before interpolations : " << endl << energy);
-	
-				vector<double> dataX, dataY;
-				for (size_t i = repulsion_idx; i < energy.size(); ++i) { // unset everything below this value
-					dataX.push_back(__get_lower_bound(i));
-					dataY.push_back(energy[i]);
-				}
+                                dbgmsg("energies before interpolations : " << endl << energy);
+    
+                                const string idatm_type1 = help::idatm_unmask[atom_pair.first];
+                                const string idatm_type2 = help::idatm_unmask[atom_pair.second];
 
-                                vector<double> potential = Interpolation::interpolate_bspline(dataX, dataY, __step_non_bond);
+                                vector<double> dataX, dataY;
+                                for (size_t i = repulsion_idx; i < energy.size(); ++i) { // unset everything below this value
+                                        dataX.push_back(__get_lower_bound(i));
+                                        dataY.push_back(energy[i]);
+                                }
+
+                                Interpolation::BSplineFit BSFited(dataX, dataY);
+                                vector<double> potential = BSFited.interpolate_bspline(dataX.front(), dataX.back(), __step_non_bond);
+
+                                // Extrapolate extra points to fill the gap between the upperbound and the cutoff
+                                // IE. If the cutoff is 6, the upperbound is 5.9, so 5.91 5.92, etc need to be added
+                                // This is done here to avoid tainting the spline fitting
+                                const double potential_deriv_1 = (*(potential.rbegin() + 0) - *(potential.rbegin() + 1)) * __step_non_bond;
+                                const double potential_deriv_2 = (*(potential.rbegin() + 1) - *(potential.rbegin() + 2)) * __step_non_bond;
+                                const double potential_2_deriv = (potential_deriv_1 - potential_deriv_2) * __step_non_bond;
+
+                                for ( size_t i = 0; i < (__step_in_file / __step_non_bond); ++i) {
+                                        potential.push_back(potential.back() + (potential_deriv_1 + i*potential_2_deriv ));
+                                }
 
                                 // add repulsion term by fitting 1/x**12 function to slope points
                                 const double x1 = __get_lower_bound(*slope.begin());
@@ -433,19 +448,22 @@ namespace Score {
                                 double coeffA, coeffB, WSSR;
                                 std::tie(coeffA, coeffB, WSSR) = fit_range_power_function_fast(r, pot);
 
-				dbgmsg("atom1 = " << idatm_type1
-					<< " atom2 = " << idatm_type2
-					<< " coeffA = " << coeffA
-					<< " coeffB = " << coeffB
-					<< " WSSR = " << WSSR);
+                                dbgmsg("atom1 = " << idatm_type1
+                                        << " atom2 = " << idatm_type2
+                                        << " coeffA = " << coeffA
+                                        << " coeffB = " << coeffB
+                                        << " WSSR = " << WSSR);
 
 
-				vector<double> repulsion;
-				for (double xi = 0; xi < dataX.front(); xi += __step_non_bond) {
-					const double yi = coeffA / pow(xi, 12) + coeffB;
-					repulsion.push_back(std::isinf(yi) ? 10 * coeffA / pow(xi + __step_non_bond , 12) + coeffB : yi);
-				}
-				dbgmsg("repulsion.size() = " << repulsion.size());
+                                vector<double> repulsion;
+                                // Add starting point for x=0 (so it's its not NaN).
+                                repulsion.push_back(10 * coeffA / pow(__step_non_bond , 12) + coeffB);
+
+                                // No longer loop over a double as this caused an extra point to be calculated
+                                for (size_t i = 1; i < std::floor( __dist_cutoff / __step_non_bond) - potential.size() + 1; ++i) {
+                                        const double yi = coeffA / pow(i * __step_non_bond, 12) + coeffB;
+                                        repulsion.push_back(std::isinf(yi) ? 10 * coeffA / pow(i * (__step_non_bond+1) , 12) + coeffB : yi);
+                                }
 #ifndef NDEBUG
 				for (size_t i = 0; i < repulsion.size(); ++i)
 					dbgmsg("i = " << i << " repulsion = " << repulsion[i]);
@@ -461,10 +479,10 @@ namespace Score {
 
 				const double rep_good = repulsion.back();
 				const double k0 = (potential.front() - rep_good) / w;
-				for (int k = 1; k < w; ++k) {
+				for (int k = 1; k <= w; ++k) {
 					repulsion.push_back(rep_good + k0 * k);
 				}
-	
+
 				// add repulsion term before bsplined potential
 				potential.insert(potential.begin(), repulsion.begin(), repulsion.end());
 
@@ -481,7 +499,7 @@ namespace Score {
 			} // END of try
 			catch (InterpolationError &e) {
 				dbgmsg(e.what());
-				const int n = (int) floor(__dist_cutoff / __step_non_bond) + 1;
+				const int n = (int) std::floor( __dist_cutoff / __step_non_bond) + 1;
 				dbgmsg("n = " << n);
 				__energies[atom_pair].assign(n, 0.0);
 			}
