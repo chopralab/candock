e9ddd65d3418383c0788c1591175b83a7cdf0c82
Jonathan Fine <finej@purdue.edu>
2019-07-23 16:07:02 -0400

    Add License

--- lib/cluster/greedy.cpp
+++ lib/cluster/greedy.cpp
@@ -1,3 +1,18 @@
+/* This is greedy.cpp and is part of CANDOCK
+ * Copyright (c) 2016-2019 Chopra Lab at Purdue University, 2013-2016 Janez Konc at National Institute of Chemistry and Samudrala Group at University of Washington
+ *
+ * This program is free for educational and academic use
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
 #include "candock/cluster/greedy.hpp"
 #include <exception>
 #include <iostream>
