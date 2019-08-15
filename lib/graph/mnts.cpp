175b4caa71c331944c7973e4f5d6e82df88c32dc
Jonathan Fine <finej@purdue.edu>
2019-08-15 14:43:41 -0400

    Add license

--- lib/graph/mnts.cpp
+++ lib/graph/mnts.cpp
@@ -1,3 +1,24 @@
+/*  Multi-neighborhood tabu search for the maximum weight clique problem
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * This program demonstrates the use of the heuristic algorithm for solving
+ * the problem of the maximum weight clique problem. For more information about this algorithm, 
+ * please visit the website http://www.info.univ-angers.fr/pub/hao/mnts_clique.html or email to: wu@info-univ.angers.fr.
+ */
+
 #include "mnts.hpp"
 void MNTS::initialize() {
 	srand( (unsigned) time( NULL ) );
